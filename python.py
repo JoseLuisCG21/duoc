9fd508dbb0126e8e5860d98d37fc8a91f9988f66
JoseLuisCG21 <jos.cisternas@duocuc.cl>
2025-05-06 10:26:20 -0400

    nose

--- python.py
+++ python.py
@@ -1,3 +1,5 @@
+
+
 # print("ingrese su nombre")
 # nombre=imput()
 # print("ingrese su edad")
@@ -38,29 +40,61 @@
 
 
 
+# ingresos=0
+# puntaje=0
+
+# ingresos=float(input("Ingrese su cantidad de ingresos: "))
+
+# nivel_educacional=int(input('''ingrese su nivel educacional: 
+#                         1.- Enseñanza basica x1
+#                         2.- Enseñanza media x1.3
+#                         3.- Enseñanza superior1.5 '''))
+
+# nacionalidad=int(input('''ingrese su nacionalidad: 
+#                    1.- Chileno
+#                    2.- Extranjero'''))
+
+
+# if 500000 <= ingresos < 1000000:
+#     print(f"Sus ingresos {ingresos} y puntaje son {puntaje}")
+#     puntaje+= 300000
+# elif 1000000 <= ingresos < 1500000:
+#     print(f"Sus ingresos {ingresos} y puntaje son {puntaje}")
+#     puntaje+= 650000
+# elif 1500001 < ingresos < 999999999:
+#     print(f"Sus ingresos {ingresos} y puntaje son {puntaje}")
+#     puntaje+=1000000
+# else:
+#     puntaje=0
+
+# if nivel_educacional==1:
+#     puntaje*=1.0
+# elif nivel_educacional==2:
+#     puntaje*=1.3
+# elif nivel_educacional==3:
+#     puntaje*=1.5
+# else:
+#     puntaje=0
+
+# if nacionalidad==1:
+#     puntaje+=300000
+# elif nacionalidad==2:
+#     puntaje=0
+    
+
+# print(f"Sus ingresos{ingresos} y su puntajes {puntaje} son un total de: {ingresos+puntaje}")
 
 
 
 
+import random
+
+n1=int(input("ingrese un numero"))
+print("El segundo numero no puede ser menor")
+n2=int(input("Ingrese un numero"))
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+numAzar=random.randint(n1,n2)
 
 
 
