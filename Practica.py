c22c303c9d5910f4d9fa3ff9e5c091c9ac4c382f
AlejandroCG21 <jos.cisternas@duocuc.cl>
2025-06-21 12:24:46 -0400

    Repaso

---
+++ Practica.py
@@ -0,0 +1,169 @@
+# email = input("Ingrese su email: ")
+# if "@" in email:
+#     print("La variable tiene formato email")
+# else:
+#     print("La variable NO tiene formato email")
+
+# while True:
+#     try:
+#         pin = int(input("Ingrese su PIN: "))
+#         break
+#     except Exception:
+#         print("Error, Ingrese numeros enteros")
+        
+        
+        
+# clave = input("Ingrese su clave: ")
+
+# if len(clave)>=5:
+#     print("La clave tiene el largo correcto")
+# else:
+#     print("La clave debe tener al menos 5 caracteres")
+
+# if len(clave)>=5 and len(clave)<=12:
+#     print("La clave tiene el largo correcto")
+# else:
+#     print("La clave debe tener minimo 5 y maximo 12 caracteres")
+
+
+
+# tieneMayus = False
+# tieneMinus = False
+# tieneNumero = False
+
+# #Se hace un recorrido en cada letra de la clave
+# for letra in clave:
+#     #verifico si la letra es mayuscula
+#     if letra.isupper():
+#         tieneMayus = True
+#     #verifico que la letra es minuscula
+#     if letra.islower():
+#         tieneMayus = True
+#     #Verifico si tengo un numero
+#     if letra.isdigit():
+#         tieneNumero = True
+
+# if tieneMayus:
+#     print("Tiene por lo menos una mayuscula")
+# else: 
+#     print(" NO tiene por lo menos una mayuscula")
+# if tieneMinus:
+#     print("Tiene por lo menos una minuscula")
+# else: 
+#     print(" NO tiene por lo menos una minuscula")
+
+# if tieneMayus and tieneMinus and tieneNumero:
+#     print("La clave esta bien")
+# else:
+#     print("Debe intentar nuevamente")
+
+
+
+# Repaso de listas y diccionarios
+
+perros = [
+        1:{"Nombre" : "Droopy",
+            "Raza" : "Dog hount",
+            "Codigo" : "Dphh06"}
+    ]
+def valida_pass(clave):    
+    Mayuscula=False
+    Minuscula=False
+    Numero=False
+
+    for palabra in clave:
+        if palabra.isupper():
+            Mayuscula=True
+        if palabra.islower():
+            Minuscula=True
+        if palabra.isdigit():
+            Número=True
+    if (Mayuscula and Minuscula and Numero) and len(clave)==6:
+        print("la clave está bien escrita")
+        return True
+    else:
+        print("la clave está mal escrita")
+        return False
+
+def ingresa_perro(dict):
+    nombre = input("Ingrese un Nombre: ")
+    raza = input("Ingrese su raza: ")
+    codigo = input("Ingrese su codigo: ")
+    if valida_pass(codigo):
+        largo = list(dict.keys())[-1]
+        dict[largo+1] = {"Nombre" : nombre,
+                            "Raza" : raza ,
+                            "Codigo" : codigo}
+
+def mostrar_perros(dict):
+    for key, perro in dict.items():
+                print(key, perro)
+
+def act_perros(dict):
+    mostrar_perros(perros)
+    act = int(input("Seleccione el perro que va a actualizar: "))
+    while True:
+        print('''
+            1.- Nombre
+            2.- Raza
+            3.- Codigo
+            4.- Salir
+            ''')
+        dato = int(input("¿Que dato va a actualizar?: "))
+        match dato:
+            case 1:
+                n = input("Ingrese el nuevo nombre: ")
+                perros[act]["nombre"] = n
+            case 2:
+                n = input("Ingrese la nueva raza: ")
+                perros[act]["raza"] = n
+            case 3:
+                n = input("Ingrese el nuevo codigo: ")
+                if valida_pass(n):
+                    perros[act][]
+                    
+                perros[act]["codigo"] = n
+            case 4:
+                print("Saliendo...")
+                break
+            case _:
+                perros[act]
+
+def borrar_perros(dict):
+        borrar = int(input("Seleccione el perro a borrar: "))
+        del perros[borrar]
+
+while True:
+    try:
+        print('''
+              1.- Registrar un perro
+              2.- Mostrar perros
+              3.- Actualizar perro
+              4.- Borrar perro
+              5.- Salir
+              ''')
+        
+    op=int(input("Seleccione una opción: "))
+    match op:
+        case 1:
+            ingresa_perro(dict)
+        case 2:
+            mostrar_perros(perros)
+        case 3:
+            act_perros(perros)
+        case 4:
+            borrar_perros(dict)
+            
+        case 5:
+            print("Saliendo...")
+            break
+        case _:
+            print("Opción invalida")
+            
+    except Exception:
+    print("El error es: ")
+        
+        
+        
+
+    
--- clases.py
+++ clases.py
@@ -1,96 +1,96 @@
-# #Uso y explicacion del match
-# import random
+#Uso y explicacion del match
+import random
 
-# def suma():
-#     n1=int(input("ingrese un numero"))
-#     n2=int(input("ingrese otro numero"))
-#     print(f"El resultado de la suma es: {n1+n2}")
-#     suma()
-# def resta():
-#     n1=int(input("ingrese un numero"))
-#     n2=int(input("ingrese otro numero"))
-#     print(f"El resultado de la resta es: {n1-n2}")
-#     resta()
-# def multiplicacion():
-#     n1=int(input("ingrese un numero"))
-#     n2=int(input("ingrese otro numero"))
-#     print(f"El resultado de la multiplicacion es: {n1*n2}")
-#     multiplicacion()
-# def division():
-#     n1=int(input("ingrese un numero"))
-#     n2=int(input("ingrese otro numero"))
-#     print(f"El resultado de la division es: {n1/n2}")
-#     division()
+def suma():
+    n1=int(input("ingrese un numero"))
+    n2=int(input("ingrese otro numero"))
+    print(f"El resultado de la suma es: {n1+n2}")
+    suma()
+def resta():
+    n1=int(input("ingrese un numero"))
+    n2=int(input("ingrese otro numero"))
+    print(f"El resultado de la resta es: {n1-n2}")
+    resta()
+def multiplicacion():
+    n1=int(input("ingrese un numero"))
+    n2=int(input("ingrese otro numero"))
+    print(f"El resultado de la multiplicacion es: {n1*n2}")
+    multiplicacion()
+def division():
+    n1=int(input("ingrese un numero"))
+    n2=int(input("ingrese otro numero"))
+    print(f"El resultado de la division es: {n1/n2}")
+    division()
 
 
-# def calcu():
-#     while True:
-#         op=int(input(''' Ingrese una opcion
-#                 1.- suma
-#                 2.- resta
-#                 3.- multiplicacion
-#                 4.- division
-#                 5.- salir'''))
+def calcu():
+    while True:
+        op=int(input(''' Ingrese una opcion
+                1.- suma
+                2.- resta
+                3.- multiplicacion
+                4.- division
+                5.- salir'''))
 
-#         match op:
-#             case 1:
-#                 print("suma")
-#                 suma()
-#             case 2:
-#                 print("resta")
-#                 resta()
-#             case 3:
-#                 print("multiplicacion")
-#                 multiplicacion()
-#             case 4:
-#                 print("division")
-#                 division()
-#             case 5:
-#                 print("Saliendo...")
-#                 break
-#             case _:
-#                 print("opcion invalida")
+        match op:
+            case 1:
+                print("suma")
+                suma()
+            case 2:
+                print("resta")
+                resta()
+            case 3:
+                print("multiplicacion")
+                multiplicacion()
+            case 4:
+                print("division")
+                division()
+            case 5:
+                print("Saliendo...")
+                break
+            case _:
+                print("opcion invalida")
 
-# calcu()
+calcu()
 
-# #Perros
-# import random
-# while True:
-#     try:
-#         perros=int(input("Ingrese la cantidad de perros: "))
-#         cuota=int(input("ingrese la cantidad minima de cuota por conejos: "))
-#         cumplen=0
-#         conejos=random.randint(0,10)
-#         print("Los perros fueron a cazar!!")
+#Perros
+import random
+while True:
+    try:
+        perros=int(input("Ingrese la cantidad de perros: "))
+        cuota=int(input("ingrese la cantidad minima de cuota por conejos: "))
+        cumplen=0
+        conejos=random.randint(0,10)
+        print("Los perros fueron a cazar!!")
 
-#         for p in range(perros):
-#             if cuota>=conejos:
-#                 print(f"El perro {p+1} trajo {conejos} conejos")
-#                 cumplen+=1
-#             else:
-#                 print(f"El perro {p+1} trajo {conejos} conejos. se queda sin filete")
+        for p in range(perros):
+            if cuota>=conejos:
+                print(f"El perro {p+1} trajo {conejos} conejos")
+                cumplen+=1
+            else:
+                print(f"El perro {p+1} trajo {conejos} conejos. se queda sin filete")
             
-#         print(f"El total de perros que cumplen con la cuota fue: {cumplen}")
-#         print(f"El total de perros que no cumplen con la cuota fue: {perros-cumplen}")
+        print(f"El total de perros que cumplen con la cuota fue: {cumplen}")
+        print(f"El total de perros que no cumplen con la cuota fue: {perros-cumplen}")
 
-#     except Exception:
-#         print("Solo debes poner numeros enteros")
-
-
+    except Exception:
+        print("Solo debes poner numeros enteros")
 
 
-# #rango de numero 0 a 100 evaluando errores
 
-# while True:
-#     try:
-#         numero = int(input("ingrese un numero entre 0 y 100"))
-#         if numero < 0 or numero > 100:
-#             print("Error fuera de rango")
-#         else:
-#             break
 
-#     except ValueError:
-#         print("Error solo se permite numeros enteros")
+#rango de numero 0 a 100 evaluando errores
+
+while True:
+    try:
+        numero = int(input("ingrese un numero entre 0 y 100"))
+        if numero < 0 or numero > 100:
+            print("Error fuera de rango")
+        else:
+            break
+
+    except ValueError:
+        print("Error solo se permite numeros enteros")
 
 
 
